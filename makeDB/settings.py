d25b961b21f86aba2c66f638b5a3dc3b6f04f78d
leesungwoo <coms3319@naver.com>
2016-11-26 16:41:56 +0900

    final

--- makeDB/settings.py
+++ makeDB/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd=ww!*ri!ds0zoyh121_z!&l$n254pemzzix6i2jviku0+khk2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SungwooLee.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
