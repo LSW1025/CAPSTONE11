19e30192bdf7958f6de82a7ad93677336758f71e
leesungwoo <coms3319@naver.com>
2016-11-29 17:07:33 +0900

    add InitWordd in admin

--- apps1/admin.py
+++ apps1/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Word, Checkdup
+from .models import Word, Checkdup, InitWord
 
 admin.site.register(Word)
 admin.site.register(Checkdup)
+admin.site.register(InitWord)
 # Register your models here.
